16e52525a9b0001d88c49324cf9e3cee6ad85970
TyBaron <tbc35@txstate.edu>
2018-02-28 00:42:21 -0600

    Added tile highlighting among other things

--- exampleGUI/src/gui/Board.java
+++ exampleGUI/src/gui/Board.java
@@ -23,12 +23,16 @@ public class Board {
 	
 	private Color whiteTiles = Color.decode("#edebd3");
 	private Color blackTiles = Color.decode("#545239");
+	private Color blueTiles = Color.decode("#09F6BB");
+	
 	public static int selected = -1; //when no piece is selected equals -1
 	chessGame newGame = new chessGame();
 	
 	private final JFrame gameFrame;
 	private final BoardPanel boardPanel;
 	private final GravePanel grave; 
+	public int graveCount = 0;
+	public boolean[] highlighted = new boolean[64];
 	
 	private final static Dimension BOARD_PANEL = new Dimension(80, 80);
 	private final static Dimension TILE_PANEL = new Dimension(20, 20);
@@ -64,12 +68,13 @@ private class GravePanel extends JPanel{
 	
 	
 private class BoardPanel extends JPanel{
-	final List<TilePanel> boardTiles; 
+	final List<TilePanel> boardTiles;
 	
 	BoardPanel(final GravePanel grave){
 		super(new GridLayout(8,8));
 		this.boardTiles = new ArrayList<>();
 		for(int i = 0; i < 64; i++) {
+			highlighted[i] = false;
 			final TilePanel tilePanel = new TilePanel(this, grave, i);
 			this.boardTiles.add(tilePanel);
 			add(tilePanel);
@@ -101,32 +106,52 @@ private class TilePanel extends JPanel{
 	  addMouseListener(new MouseAdapter() {
 		  @Override
 		  public void mousePressed(MouseEvent e) {
+			  
 			 if (Board.selected==-1) {
 				 //piece select
 				 if(newGame.p[tileID].getName() != null && 
 						 newGame.p[tileID].getAlly() == newGame.turn) {
 				     Board.selected = tileID;
+				     highlightTile(Board.selected);
 				 }
 				 else 
 					 Board.selected = -1;
 			 }
+			 
+			
+			 else if(newGame.p[tileID].getLocation()==tileID) {
+				 //deselect when moved to same spot
+				 Board.selected = -1;
+			 }
 			 else if(newGame.p[tileID].getAlly() == newGame.p[Board.selected].getAlly())
 				 System.out.println("cant take own piece");
-			 else if ((Board.selected!=-1 && newGame.p[tileID].getName()!=null)) {
+			 else if ((Board.selected!=-1 && newGame.p[tileID].getName()!=null 
+					 && newGame.p[Board.selected].validMove(Board.selected, tileID) == true)) {
 				 //take piece 
 				 grave.graveYardAdd(newGame.p[tileID].getName());
-				 newGame.p[tileID].setTo(newGame.p[Board.selected]);
-				 newGame.p[Board.selected].setEmpty();
-				 Board.selected = -1;
+				 newGame.holder = newGame.grave[graveCount];
+				 newGame.grave[graveCount] = newGame.p[Board.selected];
+				 newGame.p[tileID] = newGame.grave[graveCount];
+				 newGame.p[Board.selected] = newGame.holder;
 				 newGame.changeTurn();
 				 newGame.turnUp();
+				 Board.selected = -1;
+				 graveCount++;
 			 }
-			 else{				 
-				 newGame.p[tileID].setTo(newGame.p[Board.selected]);
-				 newGame.p[Board.selected].setEmpty();
-				 Board.selected = -1;
-				 newGame.changeTurn();
-				 newGame.turnUp();
+			 else{
+		 
+				 if (newGame.p[Board.selected].validMove(Board.selected, tileID) == false) {
+					 //invalid move do nothing
+				 }
+				 else {
+				   //move to empty 
+				   newGame.holder = newGame.p[Board.selected];
+				   newGame.p[Board.selected] = newGame.p[tileID];
+				   newGame.p[tileID] = newGame.holder;
+				   Board.selected = -1;
+				   newGame.changeTurn();
+				   newGame.turnUp();
+				 }
 				 
 			 }
 			 SwingUtilities.invokeLater(new Runnable() {
@@ -158,11 +183,22 @@ private class TilePanel extends JPanel{
 	}
 		
 	private void assignTileColor() {
-		if((this.tileID/8)% 2==0 )
+		if(highlighted[this.tileID] == true) {
+			setBackground(blueTiles);
+			highlighted[this.tileID] = false;
+		}
+		else if((this.tileID/8)% 2==0 )
 		    setBackground(this.tileID %2 == 0 ? whiteTiles : blackTiles);
 		else 
 			setBackground(this.tileID %2 == 0 ? blackTiles : whiteTiles);
 	
+	}
+	private void highlightTile(int selected) {
+		for(int i = 0; i<64; i++) {
+			if(newGame.p[selected].validMove(selected, i) == true) {
+			   highlighted[i] = true;
+			}
+	    }
 	}
 		
   }

a1bc138db2b27dd1f306d420a79f88238a5e4997
TyBaron <tbc35@txstate.edu>
2018-02-28 00:41:13 -0600

    Update chessGame.java
    All unique pieces are now their own object

--- exampleGUI/src/chessGame/chessGame.java
+++ exampleGUI/src/chessGame/chessGame.java
@@ -1,5 +1,11 @@
 package chessGame;
+import Pieces.Bishop;
+import Pieces.King;
+import Pieces.Knight;
+import Pieces.Pawn;
 import Pieces.Piece;
+import Pieces.Queen;
+import Pieces.Rook;
 
 
 public class chessGame {
@@ -8,85 +14,67 @@ public class chessGame {
 	public int turn; //0=white 1=black
 	public int turnNumber;
 	public Piece[] p = new Piece[64];
+	public Piece[] grave = new Piece[32];
+	public Piece holder = new Piece();
 	
 	
 	public chessGame(){
+		
 	
     for (int i = 0; i<64; i++) {
-    	p[i] = new Piece();
     	if (i==0 ||i == 7) {
-    		p[i].setAlly(1);
+    		p[i] = new Rook(1);
     		p[i].moveLocation(i);
-    		p[i].setName("Black Rook");
-    		p[i].setImageName("BRook");
     	}
     	else if (i==1||i==6){
-    		p[i].setAlly(1);
+    		p[i] = new Knight(1);
     		p[i].moveLocation(i);
-    		p[i].setName("Black Knight");
-    		p[i].setImageName("BKnight");
     	}
     	else if (i==2||i==5){
-    		p[i].setAlly(1);
+    		p[i] = new Bishop(1);
     		p[i].moveLocation(i);
-    		p[i].setName("Black Bishop");
-    		p[i].setImageName("BBishop");
     	}
     	else if (i==3){
-    		p[i].setAlly(1);
+    		p[i] = new Queen(1);
     		p[i].moveLocation(i);
-    		p[i].setName("Black Queen");
-    		p[i].setImageName("BQueen");
     	}
     	else if (i==4){
-    		p[i].setAlly(1);
+    		p[i] = new King(1);
     		p[i].moveLocation(i);
-    		p[i].setName("Black King");
-    		p[i].setImageName("BKing");
     	}
     	else if (i==63 ||i == 56){
-    		p[i].setAlly(0);
+    		p[i] = new Rook(0);
     		p[i].moveLocation(i);
-    		p[i].setName("White Rook");
-    		p[i].setImageName("WRook");
     	}
     	else if (i==62||i==57){
-    		p[i].setAlly(0);
+    		p[i] = new Knight(0);
     		p[i].moveLocation(i);
-    		p[i].setName("White Knight");
-    		p[i].setImageName("WKnight");
     	}
     	else if (i==61||i==58){
-    		p[i].setAlly(0);
+    		p[i] = new Bishop(0);
     		p[i].moveLocation(i);
-    		p[i].setName("White Bishop");
-    		p[i].setImageName("WBishop");
     	}
     	else if (i==59){
-    		p[i].setAlly(0);
+    		p[i] = new Queen(0);
     		p[i].moveLocation(i);
-    		p[i].setName("White Queen");
-    		p[i].setImageName("WQueen");
     	}
     	else if (i==60){
-    		p[i].setAlly(0);
+    		p[i] = new King(0);
     		p[i].moveLocation(i);
-    		p[i].setName("White King");
-    		p[i].setImageName("WKing");
     	}
     	
     	else if (i>7 && i <16){
-    		p[i].setAlly(1);
+    		p[i] = new Pawn(1);
     		p[i].moveLocation(i);
-    		p[i].setName("Black Pawn");
-    		p[i].setImageName("BPawn");
     	}
     	else if(i>47 && i < 56){
-    		p[i].setAlly(0);
+    		p[i] = new Pawn(0);
     		p[i].moveLocation(i);
-    		p[i].setName("White pawn");
-    		p[i].setImageName("WPawn");
     	}
+    	else 
+    		p[i] = new Piece();
+    	if(i<32)
+    	    grave[i] = new Piece();
     }
     turn = 0; 
     turnNumber = 0;
@@ -101,4 +89,6 @@ public class chessGame {
 	  turnNumber++;
   }
 }
+
+
 

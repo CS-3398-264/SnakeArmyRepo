811dec8d3e053e9d2fa826646e12a78ecf23f263
TyBaron <tbc35@txstate.edu>
2018-02-28 17:41:25 -0600

    fixed boundary issue with king movement

--- exampleGUI/src/Pieces/King.java
+++ exampleGUI/src/Pieces/King.java
@@ -19,8 +19,11 @@ public class King extends Piece{
 	public boolean validMove(int start, int end) {
 		boolean valid = false;
 		int product = end-start;
-		if(product == -8 || product == 8 || product == 1 || product == -1 
-				|| product == 9 || product == 7 || product == -7 || product == -9) {
+		if((start%8 == 0 && (product == -1 || product == 7 || product == -9 )
+				|| start%8==7 && (product == 1 || product == -7 || product == 9)))
+			valid = false;
+		else if((product == -8 || product == 8 || product == 1 || product == -1 
+				|| product == 9 || product == 7 || product == -7 || product == -9)) {
 			valid = true;
 		}
 			

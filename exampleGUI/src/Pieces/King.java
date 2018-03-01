35526e959d7b57bddf8f98b3e06cf4afc74dffb6
Nano95 <30358490+Nano95@users.noreply.github.com>
2018-02-28 18:21:04 -0600

    Added explanation of castling for king

--- exampleGUI/src/Pieces/King.java
+++ exampleGUI/src/Pieces/King.java
@@ -29,5 +29,11 @@ public class King extends Piece{
 			
 		return valid; 
 	}
+	
+	/* Castling: possible if knight has not moved at all, nor the rook, and the spaces between the 2 are empty. 
+	 * The positions were they are placed, are if castle with left rook... King moves 2 to the left, and Rook on his right
+	 * If it castles on the right, again, moves 2 to the right, but rook is on his left. 
+	 * If those conditions are met, then when the King is selected, the areas highlighted for valid moves for the king
+	 * Include the 2 spots on the left and right of the king.
 
 }

1264c6492634fc1d63891ead44807c5a314dfc48
TyBaron <tbc35@txstate.edu>
2018-03-03 00:19:04 -0600

    Got rid of moveLocation

--- exampleGUI/src/chessGame/chessGame.java
+++ exampleGUI/src/chessGame/chessGame.java
@@ -24,52 +24,40 @@ public class chessGame {
     for (int i = 0; i<64; i++) {
     	if (i==0 ||i == 7) {
     		p[i] = new Rook(1);
-    		p[i].moveLocation(i);
     	}
     	else if (i==1||i==6){
     		p[i] = new Knight(1);
-    		p[i].moveLocation(i);
     	}
     	else if (i==2||i==5){
     		p[i] = new Bishop(1);
-    		p[i].moveLocation(i);
     	}
     	else if (i==3){
     		p[i] = new Queen(1);
-    		p[i].moveLocation(i);
     	}
     	else if (i==4){
     		p[i] = new King(1);
-    		p[i].moveLocation(i);
     	}
     	else if (i==63 ||i == 56){
     		p[i] = new Rook(0);
-    		p[i].moveLocation(i);
     	}
     	else if (i==62||i==57){
     		p[i] = new Knight(0);
-    		p[i].moveLocation(i);
     	}
     	else if (i==61||i==58){
     		p[i] = new Bishop(0);
-    		p[i].moveLocation(i);
     	}
     	else if (i==59){
     		p[i] = new Queen(0);
-    		p[i].moveLocation(i);
     	}
     	else if (i==60){
     		p[i] = new King(0);
-    		p[i].moveLocation(i);
     	}
     	
     	else if (i>7 && i <16){
     		p[i] = new Pawn(1);
-    		p[i].moveLocation(i);
     	}
     	else if(i>47 && i < 56){
     		p[i] = new Pawn(0);
-    		p[i].moveLocation(i);
     	}
     	else 
     		p[i] = new Piece();
@@ -89,6 +77,3 @@ public class chessGame {
 	  turnNumber++;
   }
 }
-
-
-
